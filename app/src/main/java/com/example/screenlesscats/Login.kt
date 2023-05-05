b815afa7313d6633c289c4d6b36aaad54ae37928
jpg <92806924+Janzokk@users.noreply.github.com>
2023-05-05 19:58:12 +0200

    Added implementations to Firebase and Created Login Activity XML and Started signIn method

---
+++ app/src/main/java/com/example/screenlesscats/Login.kt
@@ -0,0 +1,56 @@
+package com.example.screenlesscats
+
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class Login : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var email: TextInputEditText
+    private lateinit var password: TextInputEditText
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        email = findViewById<TextInputEditText>(R.id.email)
+
+        password = findViewById<TextInputEditText>(R.id.password)
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        auth.signInWithEmailAndPassword(email.toString(), password.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
