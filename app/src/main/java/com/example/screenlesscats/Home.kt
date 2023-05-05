31548215b8154b67b355b0387e1942d01b4f60ee
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2023-05-05 19:56:55 +0200

    Added bottom bar

--- app/src/main/java/com/example/screenlesscats/Home.kt
+++ app/src/main/java/com/example/screenlesscats/Home.kt
@@ -2,10 +2,42 @@ package com.example.screenlesscats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationBarView
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val homeFragment = HomeFragment()
+
+        NavigationBarView.OnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.item_time -> {
+                    Log.d("DEBUG", "HOME CLICKED")
+                    true
+                }
+                R.id.item_home -> {
+                    Log.d("DEBUG", "HOME CLICKED")
+                    setCurrentFragment(homeFragment)
+                    true
+                }
+                R.id.item_cats -> {
+                    Log.d("DEBUG", "HOME CLICKED")
+                    true
+                }
+                else -> false
+            }
+        }
     }
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+
+
 }
