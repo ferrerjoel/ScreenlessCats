cf5b2dcb6ea69dd1fb32efdc42fc44d4cce1095f
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2023-05-08 19:54:55 +0200

    Added time selection on time manager and top bar on home activity

--- app/src/main/java/com/example/screenlesscats/Home.kt
+++ app/src/main/java/com/example/screenlesscats/Home.kt
@@ -1,18 +1,20 @@
 package com.example.screenlesscats
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Home : AppCompatActivity() {
     private lateinit var bottomNavigationBar: BottomNavigationView
+    private lateinit var topAppBar: MaterialToolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         bottomNavigationBar = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        topAppBar = findViewById(R.id.top_app_bar)
 
         val homeFragment = HomeFragment()
         val timeFragment = TimeManagementFragment()
@@ -26,14 +28,31 @@ class Home : AppCompatActivity() {
             when(item.itemId) {
                 R.id.item_time -> {
                     setCurrentFragment(timeFragment)
+                    topAppBar.title = "Time management"
                     true
                 }
                 R.id.item_home -> {
                     setCurrentFragment(homeFragment)
+                    topAppBar.title = "Home"
                     true
                 }
                 R.id.item_cats -> {
                     setCurrentFragment(catsFragment)
+                    topAppBar.title = "Cats"
+                    true
+                }
+                else -> false
+            }
+        }
+
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.about_us_option -> {
+                    // Handle edit text press
+                    true
+                }
+                R.id.sign_out_option -> {
+                    // Handle favorite icon press
                     true
                 }
                 else -> false
--- app/src/main/java/com/example/screenlesscats/TimeManagementFragment.kt
+++ app/src/main/java/com/example/screenlesscats/TimeManagementFragment.kt
@@ -1,9 +1,74 @@
 package com.example.screenlesscats
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 
-// TODO: Rename parameter arguments, choose names that match
 
 class TimeManagementFragment:Fragment(R.layout.fragment_time_management) {
 
+    private lateinit var setTimeButton: Button
+    private lateinit var limitTimeTv: TextView
+
+    private lateinit var timePicker: MaterialTimePicker
+    private var limitHours: Int = 0
+    private var limitMinutes: Int = 0
+
+    private lateinit var sharedPreferences : SharedPreferences
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sharedPreferences = context?.getSharedPreferences("LimitTimeVariablesLocal", Context.MODE_PRIVATE)!!
+
+        limitHours = sharedPreferences.getInt("limitHours", 0)
+        limitMinutes = sharedPreferences.getInt("limitMinutes", 0)
+
+        limitTimeTv = view.findViewById(R.id.timeLimitTv)
+        setTextViewTime()
+
+        timePicker =
+            MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .setHour(1)
+                .setMinute(0)
+                .setTitleText("Select daily screen time")
+                .build()
+
+        timePicker.addOnPositiveButtonClickListener {
+            setLimitTime()
+        }
+
+        setTimeButton = view.findViewById(R.id.setTimeButton)
+
+        setTimeButton.setOnClickListener {
+            timePicker.show(parentFragmentManager, "tag")
+        }
+    }
+
+    private fun setLimitTime() {
+        limitHours = timePicker.hour
+        limitMinutes = timePicker.minute
+        val editor = sharedPreferences.edit()
+        editor?.putInt("limitHours", limitHours)
+        editor?.putInt("limitMinutes", limitMinutes)
+        editor?.apply()
+        setTextViewTime()
+    }
+
+    private fun setTextViewTime() {
+        if (limitHours and limitHours != 0) {
+            limitTimeTv.text = getString(R.string.TimeTemplate, limitHours, limitMinutes);
+        } else {
+            limitTimeTv.text = getString(R.string.zero_time_limit_msg)
+        }
+    }
+    
 }
