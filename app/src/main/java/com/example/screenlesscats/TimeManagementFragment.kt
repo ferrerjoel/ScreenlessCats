152dd6e1f238d29014a6abeb6c90fa40e5f7ed59
jpg <92806924+Janzokk@users.noreply.github.com>
2023-05-18 18:53:08 +0200

    Update TimeManagementFragment.kt

--- app/src/main/java/com/example/screenlesscats/TimeManagementFragment.kt
+++ app/src/main/java/com/example/screenlesscats/TimeManagementFragment.kt
@@ -338,7 +338,7 @@ class TimeManagementFragment:Fragment(R.layout.fragment_time_management) {
 
         val calendar = Calendar.getInstance()
         val toUpdate: HashMap<String, Any> = HashMap()
-        toUpdate["Date_limit_ended"] = ""+calendar.get(Calendar.YEAR) + "-" + calendar.get(Calendar.MONTH) + "-" + calendar.get(Calendar.DAY_OF_MONTH)
+        toUpdate["Date_limit_ended"] = ""+calendar.get(Calendar.YEAR) + "-" + calendar.get(Calendar.MONTH) + "-" + calendar.get(Calendar.DAY_OF_MONTH) + " " + calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE)
 
         var maxId : Long = 1
         ref.get().addOnSuccessListener {
