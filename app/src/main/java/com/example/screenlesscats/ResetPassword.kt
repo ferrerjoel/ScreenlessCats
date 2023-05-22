0a111f18ba526c0e898cb8d412db6d7e5601e33a
jpg <92806924+Janzokk@users.noreply.github.com>
2023-05-22 16:30:42 +0200

    Reset password completed

--- app/src/main/java/com/example/screenlesscats/ResetPassword.kt
+++ app/src/main/java/com/example/screenlesscats/ResetPassword.kt
@@ -1,11 +1,13 @@
 package com.example.screenlesscats
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.util.Patterns
 import android.view.View
 import android.widget.Button
+import androidx.compose.material3.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 
@@ -24,32 +26,40 @@ class ResetPassword : AppCompatActivity() {
 
         resetPwdBtn.setOnClickListener() {
             auth = FirebaseAuth.getInstance()
-            // Validate input
-            val email: String = email.text.toString()
             // Mail validation
             // If it's not a mail
-            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            if (!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()) {
                 email.error = "Invalid Mail"
             } else {
                 Log.d("DEBUG", email.text.toString())
                 auth.sendPasswordResetEmail(email.text.toString())
                     .addOnCompleteListener { task ->
                         if (task.isSuccessful) {
-                            Log.d("DEBUG", "Email sent.")
-                            Toast.makeText(
-                                this,
-                                "Reset password mail sent to your email",
-                                Toast.LENGTH_LONG
-                            ).show()
+                            com.google.android.material.snackbar.Snackbar.make(
+                                findViewById<View>(
+                                    android.R.id.content
+                                ),
+                                "Email sent",
+                                com.google.android.material.snackbar.Snackbar.LENGTH_LONG
+                            )
+                                .show()
+
                             val intent = Intent(this, Login::class.java)
                             startActivity(intent)
                             finish()
                         } else {
-                            Toast.makeText(this, "Incorrect mail", Toast.LENGTH_LONG).show()
+                            com.google.android.material.snackbar.Snackbar.make(
+                                findViewById<View>(
+                                    android.R.id.content
+                                ),
+                                "Couldn't send email",
+                                com.google.android.material.snackbar.Snackbar.LENGTH_LONG
+                            )
+                                .show()
                         }
                     }
             }
-
+        }
 
     }
 }
